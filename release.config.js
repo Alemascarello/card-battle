9ffa6189cc1096fa419b1c70a787c1d5e09df2a5
Ale <ultimoemailquefaco@gmail.com>
2025-05-27 17:52:01 -0300

    feat semantic

--- release.config.js
+++ release.config.js
@@ -1,14 +1,28 @@
+// module.exports = {
+//   bumpMinorPreMajor: true, // permite gerar 'minor' mesmo em 0.x.x
+//   preset: "conventionalcommits",
+//   types: [
+//     { type: "feat", section: "Features", release: "minor" },
+//     { type: "improv", section: "Improvements", release: "patch" },
+//     { type: "bugfix", section: "Bug Fixes", release: "patch" },
+//     { type: "hotfix", section: "Hot Fixes", release: "patch" },
+//     { type: "chore", section: "Chores", release: false },
+//   ],
+//   parserOpts: {
+//     noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+//   },
+// };
+
+// release.config.js
 module.exports = {
-  bumpMinorPreMajor: true, // permite gerar 'minor' mesmo em 0.x.x
-  preset: "conventionalcommits",
-  types: [
-    { type: "feat", section: "Features", release: "minor" },
-    { type: "improv", section: "Improvements", release: "patch" },
-    { type: "bugfix", section: "Bug Fixes", release: "patch" },
-    { type: "hotfix", section: "Hot Fixes", release: "patch" },
-    { type: "chore", section: "Chores", release: false },
+  branches: ["master"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    "@semantic-release/git",
+    "@semantic-release/github",
   ],
-  parserOpts: {
-    noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
-  },
+  preset: "conventionalcommits",
 };
