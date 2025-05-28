5b636082bf3c083a12036ff0a6d89a92b14a5a91
Ale <ultimoemailquefaco@gmail.com>
2025-05-28 10:08:30 -0300

    feat: ajusta release config

--- release.config.js
+++ release.config.js
@@ -1,10 +1,28 @@
 module.exports = {
-  bumpMinorPreMajor: true, // permite gerar 'minor' mesmo em 0.x.x
-  types: [
-    { type: "feat", section: "Features", release: "minor" },
-    { type: "improv", section: "Improvements", release: "patch" },
-    { type: "bugfix", section: "Bug Fixes", release: "patch" },
-    { type: "hotfix", section: "Hot Fixes", release: "patch" },
-    { type: "chore", section: "Chores", release: false },
+  branches: ["master"],
+  preset: "conventionalcommits",
+  parserOpts: {
+    noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+  },
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+          { type: "feat", release: "minor", section: "Features" },
+          { type: "improv", release: "patch", section: "Improvements" },
+          { type: "bugfix", release: "patch", section: "Bug Fixes" },
+          { type: "hotfix", release: "patch", section: "Hot Fixes" },
+          { type: "chore", release: false, section: "Chores" },
+        ],
+        parserOpts: {
+          headerPattern: /^(\w+)(?:\((.+)\))?: (.+)$/,
+          headerCorrespondence: ["type", "scope", "subject"],
+        },
+      },
+    ],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
   ],
 };
